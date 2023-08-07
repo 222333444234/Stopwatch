ee13b8ec9c9943cfd15b70923455a50d4889ec54
222333444234 <140579673+222333444234@users.noreply.github.com>
2023-08-07 15:33:55 +0530

    Add files via upload
    ok

---
+++ script.js
@@ -0,0 +1,53 @@
+var isstop = true;
+var sec = 0;
+var min = 0;
+var hr = 0;
+
+function start(){
+    if(isstop==true){
+        isstop = false;
+        timer();
+    }
+}
+
+function timer(){        
+
+    if(isstop==false){
+        sec++;
+        sec = parseInt(sec);
+        min = parseInt(min);
+        hr = parseInt(hr);
+        if(sec==60){
+            sec = 0;
+            min++;
+        }
+        if(min==60){
+            min = 0;
+            hr++;
+        }
+        if(sec<10){
+            sec = "0" + sec;
+        }
+        if(min<10){
+            min = "0" + min;
+        }
+        if(hr<10){
+            hr = "0" + hr;
+        }
+        stopwatch.innerHTML = hr + " : " + min + " : " + sec;
+        setTimeout('timer()',1000)
+    }
+}
+
+function stop(){
+    isstop = true;
+}
+
+function reset(){
+    isstop = true;
+    sec = 0;
+    min = 0;
+    hr = 0;
+    stopwatch.innerHTML = "00 : 00 : 00";
+}
+
